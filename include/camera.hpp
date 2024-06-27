6284b329e9400bfcc9987486f711108608be7af0
italonicacio <italonicacio@gmail.com>
2024-06-27 17:40:11 -0300

    testing one thing

--- include/camera.hpp
+++ include/camera.hpp
@@ -1,12 +1,26 @@
 #pragma once
 
 #include "ray.hpp"
+#include "utils.hpp"
+
+Vec3 RandomInUnitDisk() {
+	Vec3 p;
+	do {
+		Scalar x = Utils::Math::AutoRand();
+		Scalar y = Utils::Math::AutoRand();
+
+		p = 2.0 * Vec3({ x, y, 0.0 }) - Vec3({ 1.0, 1.0, 0.0 });
+
+	} while(p.Dot(p) >= 1.0);
+
+	return p;
+}
 
 class Camera {
 public:
-	Camera(Vec3& look_from, Vec3& look_at, Vec3 view_up, Scalar vertical_field_of_view, Scalar aspect) {
+	Camera(Vec3& look_from, Vec3& look_at, Vec3 view_up, Scalar vertical_field_of_view, Scalar aspect, Scalar focus_dist) {
 		Vec3 u, v, w;
-		Scalar theta = vertical_field_of_view * M_PI/180;
+		Scalar theta = vertical_field_of_view * M_PI/180.0;
 		Scalar half_height = std::tan(theta/2);
 		Scalar half_width = aspect * half_height;
 		this->origin = look_from;
@@ -14,10 +28,9 @@ public:
 		u = UnitVector(view_up.Cross(w));
 		v = w.Cross(u);
 
-		this->lower_left_corner = Vec3({ -half_width, -half_height, -1.0 });
-		this->lower_left_corner = origin - half_width*u - half_height*v - w;
-		this->horizontal = 2.0*half_width*u;
-		this->vertical = 2*half_height*v;
+		this->lower_left_corner = origin - half_width*focus_dist*u - half_height*focus_dist*v - focus_dist*w;
+		this->horizontal = 2.0*half_width*focus_dist*u;
+		this->vertical = 2*half_height*focus_dist*v;
 
 	}
 
